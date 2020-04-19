6d547a22f66d9d1ad6a1892a2d64359d35bcc981
owncreed93 <christiantarifer@gmail.com>
2020-04-18 19:30:00 -0500

    trabajando con parametros en las rutas

--- intro_flask/app.py
+++ intro_flask/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import request
+
 
 app = Flask(__name__)
 
@@ -8,6 +10,23 @@ def hello_world():
     return 'Hello, World!'
 
 
+@app.route('/images')
+def hello_images():
+    div = '<h1> Probando páginas pues bandido </h1>'
+    return div
+
+
+@app.route('/datos')
+def datos(nombre = "invitado", edad = 27):
+    nombre = request.args.get('nombre', nombre)
+    edad = request.args.get('edad', edad)
+    return f"<p>Hola {nombre} tu edad es de {edad}</p>"
+
+@app.route('/suma/<int:num1>/<int:num2>')
+def suma(num1 = 0, num2 = 0):
+    resultado = num1 + num2
+    return f"{num1} más {num2} igual a {resultado}"
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
